6644374562026c92382b4b3f2abe5248b78fae7e
Michal <michal.golawski.mg@gmail.com>
2025-01-25 20:07:24 +0100

    Changed uri mapping

--- src/main/java/com/example/book_rent_api/controller/BookReturnController.java
+++ src/main/java/com/example/book_rent_api/controller/BookReturnController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/return")
+@RequestMapping("/loans-api/return")
 public class BookReturnController {
 
     @Autowired
